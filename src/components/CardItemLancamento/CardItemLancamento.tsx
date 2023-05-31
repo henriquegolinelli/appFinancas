b70eaec4b6fd8b35ee40779f0f3b998f9ddcc516
enricovivan <enricovivan21@gmail.com>
2023-05-31 17:51:51 -0300

    Card Últimos Lançamentos e Saldo Total Atualizados

--- src/components/CardItemLancamento/CardItemLancamento.tsx
+++ src/components/CardItemLancamento/CardItemLancamento.tsx
@@ -2,13 +2,17 @@ import {View, StyleSheet} from 'react-native'
 import {List, ListItem, Icon, IconElement, IconProps, Text, Divider, Button} from '@ui-kitten/components'
 
 interface CardItemLancamentoProps {
-    title: string
-    description: string
+    nome: string
+    descricao: string
+    data: string
+    preco: number
 }
 
 const data = new Array(4).fill({
-    title: '15/10/2023 - Coxinha',
-    description: 'Alimentação'
+    nome: 'Coxinha',
+    data: '10/10/2020',
+    descricao: 'Alimentação',
+    preco: 250
 })
 
 export const CardItemLancamento = () => {
@@ -17,11 +21,21 @@ export const CardItemLancamento = () => {
         <Icon {...props} name='cube'></Icon>
     )
 
-    const renderItem = ({item, index}: {item: CardItemLancamentoProps; index: number}) => (
+    const renderItem = ({item, index}: {item: CardItemLancamentoProps, index: number}) => (
         <>
-            <ListItem>
+            <ListItem accessoryLeft={renderIconLeft}>
                 <View style={styles.listItemContainer}>
-                    <Button style={styles.buttonIcon} accessoryLeft={renderIconLeft} status='danger'></Button>
+                    <View>
+                        <View style={styles.container}>
+                            <Text>{item.data}</Text>
+                            <Text> - </Text>
+                            <Text style={{fontWeight: 'bold'}}>{item.nome}</Text>
+                        </View>
+                        <Text>{item.descricao}</Text>
+                    </View>
+                    <View>
+                        <Text status='danger' style={styles.itemPrice}>R$ {item.preco}</Text>
+                    </View>
                 </View>
             </ListItem>
             <Divider></Divider>
@@ -42,12 +56,20 @@ export const CardItemLancamento = () => {
 
 const styles = StyleSheet.create({
 
-    listItemContainer: {
-        flex: 1
+    container: {
+        flex: 1,
+        flexDirection: 'row'
     },
 
-    buttonIcon: {
-        width: 50,
-        height: 50,
+    listItemContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    itemPrice: {
+        fontSize: 18,
+        fontWeight: 'bold'
     }
 })
--- src/pages/Dashboard/DashboardView.tsx
+++ src/pages/Dashboard/DashboardView.tsx
@@ -1,11 +1,12 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {TopNavigation, TopNavigationAction, Layout, Text, Icon, IconProps, IconElement, Card, Button, Divider} from '@ui-kitten/components'
-import { ScrollView, View, ViewProps } from 'react-native'
+import { ScrollView, StyleSheet, View, ViewProps } from 'react-native'
 
 import { CardItemLancamento } from '../../components/CardItemLancamento/CardItemLancamento'
 import { CardItemGastoDoMes } from '../../components/CardItemGastoDoMes/CardItemGastoDoMes'
 
 import { DashboardBottomTab } from '../../components/Dashboard/BottomTab/DashboardBottomTab'
+import { useState } from 'react'
 
 const MenuIcon = (props:IconProps): IconElement => (
     <Icon {...props} name='menu' />
@@ -13,12 +14,19 @@ const MenuIcon = (props:IconProps): IconElement => (
 
 export const DashboardView = ({navigation}) => {
 
+    const [details, setDetail] = useState(false)
+
+    const toggleDetails = () => {
+        details?setDetail(false):setDetail(true);
+    }
+
     // Botão Superior Menu
     const renderDrawerAction = () => (
         <TopNavigationAction icon={MenuIcon} onPress={()=>{navigation.openDrawer()}}/>
     )
 
-    //Header Card Saldo Total
+    // CARD SALDO TOTAL
+    // Header Card Saldo Total
     const headerSaldoTotal = (props:ViewProps) => (
         <View {...props}>
             <Text category='h5' style={{textAlign: 'center'}}>Saldo Total</Text>
@@ -26,6 +34,14 @@ export const DashboardView = ({navigation}) => {
         </View>
     )
 
+    // footer Saldo Total
+    const footerSaldoTotal = (props: ViewProps) => (
+        <View {...props}>
+            <Button status='danger' onPress={toggleDetails}>Ver Detalhes</Button>
+        </View>
+    )
+
+    // CARD ULTIMOS LANCAMENTOS
     // Header ultimos lançamentos
     const headerUltimosLancamentos = (props:ViewProps) => (
         <View {...props}>
@@ -33,6 +49,7 @@ export const DashboardView = ({navigation}) => {
         </View>
     )
 
+    // CARD GASTOS DO MES
     // header Gastos do mes
     const headerGastosDoMes = (props:ViewProps) => (
         <View {...props}>
@@ -42,23 +59,46 @@ export const DashboardView = ({navigation}) => {
 
     return (
         <SafeAreaView style={{flex: 1, backgroundColor: '#2f9e41'}}>
-            <Layout style={{flex: 1}}>
+            <Layout style={styles.container}>
                 <TopNavigation accessoryRight={renderDrawerAction} alignment='center' title={props => <Text {...props} style={{color: 'white', fontSize: 18}}>DASHBOARD</Text>} style={{backgroundColor: '#2f9e41'}}/>
-                <Layout style={{flex: 1}}>
-                    <ScrollView contentContainerStyle={{flexGrow: 1, alignItems: 'center', padding: 20, gap: 20}} horizontal={false}>
+                <Layout style={styles.container}>
+                    <ScrollView contentContainerStyle={styles.scrollViewContainer} horizontal={false}>
                         
                         {/* Saldo Total */}
-                        <Card header={headerSaldoTotal} style={{width: '100%', borderRadius: 10, elevation: 10}}>
-                            <Button status='danger'>Ver Detalhes</Button>
+                        <Card header={headerSaldoTotal} style={styles.cardContainer} footer={footerSaldoTotal}>
+                            <Layout>
+                                <Text style={{textAlign: 'center', color: '#777'}}>Principal (corrente): R$ 50.000,00</Text>
+                            </Layout>
+                            <Layout style={{display: details?'flex':'none'}}>
+                                <Divider style={{marginTop: 15}}/>
+                                <Layout style={{marginTop: 10}}>
+                                    <Text style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center', color: '#555'}}>RESUMO DO MÊS</Text>
+                                    <Layout style={{marginTop: 15, gap: 5}}>
+                                        <Layout style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                                            <Text style={{fontWeight: 'bold', fontSize: 16, color: '#555'}}>Receita Mensal:</Text>
+                                            <Text status='success'>R$ 51.000,00</Text>
+                                        </Layout>
+                                        <Layout style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                                            <Text style={{fontWeight: 'bold', fontSize: 16, color: '#555'}}>Despesa Mensal:</Text>
+                                            <Text status='danger'>R$ 768,01</Text>
+                                        </Layout>
+                                        <Layout style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+                                            <Text style={{fontWeight: 'bold', fontSize: 16, color: '#555'}}>Saldo Mensal:</Text>
+                                            <Text style={{color: '#555'}}>R$ 51.000,00</Text>
+                                        </Layout>
+                                    </Layout>
+                                </Layout>
+                            </Layout>
+                            
                         </Card>
 
                         {/* Últimos Lançamentos */}
-                        <Card header={headerUltimosLancamentos} style={{width: '100%', borderRadius: 10, elevation: 10}}>
+                        <Card header={headerUltimosLancamentos} style={styles.cardContainer}>
                             <CardItemLancamento></CardItemLancamento>
                         </Card>
 
                         {/* Gastos do Mês */}
-                        <Card header={headerGastosDoMes} style={{width: '100%', borderRadius: 10, elevation: 10}}>
+                        <Card header={headerGastosDoMes} style={styles.cardContainer}>
                             <CardItemGastoDoMes></CardItemGastoDoMes>
                         </Card>
 
@@ -72,3 +112,23 @@ export const DashboardView = ({navigation}) => {
     )
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    scrollViewContainer: {
+        flexGrow: 1, 
+        alignItems: 
+        'center', 
+        padding: 20, 
+        gap: 20
+    },
+
+    cardContainer: {
+        width: '100%', 
+        borderRadius: 10, 
+        elevation: 10,
+    }
+})
